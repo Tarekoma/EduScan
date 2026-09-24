e49470b8e208def434201211677be3b1dacb8fe0
Tarek Omar <162827740+Tarekoma@users.noreply.github.com>
2026-09-24 05:56:04 +0300

    Delete lib directory

--- lib/features/auth/domain/usecases/sign_out.dart
+++
@@ -1,9 +0,0 @@
-import '../repositories/auth_repository.dart';
-
-class SignOut {
-  const SignOut(this._repository);
-
-  final AuthRepository _repository;
-
-  Future<void> call() => _repository.signOut();
-}
